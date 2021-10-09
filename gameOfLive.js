3c3d50468e7a81d03e21a9b3d6486669a0b51930
Claudia Font <claudiafontmarques@gmail.com>
2021-10-09 12:06:46 +0200

    Added cellPositionFunction

--- gameOfLive.js
+++ gameOfLive.js
@@ -1,7 +1,7 @@
 //GAME OF LIVE
 
 //1r crear tablero.
-const board = [];
+let board = [];
 function createBoard(cells) {
   for (let i = 0; i < cells; i++) {
     board.push([]);
@@ -12,9 +12,55 @@ function createBoard(cells) {
   return board;
 }
 
-console.log(createBoard(5));
+board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0],
+];
 
 //2-Analizar en que posicion esta cada elemento
+function boardPosition(currentBoard) {
+  let newBoard = [];
+  for (let i = 0; i < currentBoard.length; i++) {
+    newBoard.push([]);
+    for (let j = 0; j < currentBoard[i].length; j++) {
+      if (i === 0 && j === 0) {
+        newBoard[i].push(1);
+      } else if (i === 0 && j === currentBoard[i].length - 1) {
+        newBoard[i].push(2);
+      } else if (
+        i === currentBoard.length - 1 &&
+        j === currentBoard[i].length - 1
+      ) {
+        newBoard[i].push(3);
+      } else if (i === currentBoard.length - 1 && j === 0) {
+        newBoard[i].push(4);
+      } else if (i > 0 && i < currentBoard[i].length - 1 && j === 0) {
+        newBoard[i].push(5);
+      } else if (i === 0 && j > 0 && j < currentBoard[i].length - 1) {
+        newBoard[i].push(6);
+      } else if (
+        i > 0 &&
+        i < currentBoard[i].length - 1 &&
+        j === currentBoard.length - 1
+      ) {
+        newBoard[i].push(7);
+      } else if (
+        i === currentBoard.length - 1 &&
+        j > 0 &&
+        j < currentBoard[i].length - 1
+      ) {
+        newBoard[i].push(8);
+      } else {
+        newBoard[i].push(9);
+      }
+    }
+  }
+  return newBoard;
+}
+console.log(boardPosition(board));
 
 //3-Aplicar la funcion segun su posicionamiento i encontrar todos los vecinos vivos
 
